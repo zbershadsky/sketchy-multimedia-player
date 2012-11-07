d70488c1ffb00c490a42d4eedde3cd221eba3848
dmitri.paziy@gmail.com <dmitri.paziy@gmail.com@a71be2c3-9069-54e4-cd14-9cd77936d129>
2012-11-07 16:19:52 +0000

    Refactored video player

--- trunk/src/com/paziy/sketchy/video/controllers/VideoPlayerController.java
+++ trunk/src/com/paziy/sketchy/video/controllers/VideoPlayerController.java
@@ -1,25 +1,21 @@
 package com.paziy.sketchy.video.controllers;
 
-import com.paziy.sketchy.audio.controllers.AudioRecordsParser;
-import com.paziy.sketchy.audio.model.Audio;
+import com.paziy.sketchy.controllers.PlayerController;
+import com.paziy.sketchy.parsers.Parser;
 import com.paziy.sketchy.request.controllers.RequestController;
 import com.paziy.sketchy.request.controllers.VKUrlManager;
 import com.paziy.sketchy.video.model.Video;
+import com.paziy.sketchy.video.model.VideoFactory;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Slayer
- * Date: 07.11.12
- * Time: 13:40
- * To change this template use File | Settings | File Templates.
- */
-public class VideoPlayerController {
+public class VideoPlayerController extends PlayerController<Video>{
+    public static final String WRAPPER_TAG = "video";
     private VKUrlManager manager;
 
     public VideoPlayerController(VKUrlManager manager) {
+        super(new Parser<Video>(new VideoFactory(), WRAPPER_TAG));
         this.manager = manager;
     }
 
@@ -28,7 +24,6 @@ public class VideoPlayerController {
 
         try {
             HttpsURLConnection con = RequestController.sendRequest(manager.videoGet());
-            VideoRecordsParser parser = new VideoRecordsParser();
             result = parser.parse(con.getInputStream());
         } catch (Exception ex) {
             ex.printStackTrace();
