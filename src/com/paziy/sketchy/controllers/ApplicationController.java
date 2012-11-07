ba54f832a1e8f6c75bc3cab85dc6ff587433271c
dmitri.paziy@gmail.com <dmitri.paziy@gmail.com@a71be2c3-9069-54e4-cd14-9cd77936d129>
2012-11-07 11:35:41 +0000

    Made seeking of song possible. Added players window. It will be moved some day)

--- src/com/paziy/sketchy/controllers/ApplicationController.java
+++ src/com/paziy/sketchy/controllers/ApplicationController.java
@@ -1,15 +1,33 @@
 package com.paziy.sketchy.controllers;
 
+import com.paziy.sketchy.audio.controllers.AudioPlayerController;
 import com.paziy.sketchy.authentication.model.Authenticator;
 import com.paziy.sketchy.authentication.view.LoginWindow;
+import com.paziy.sketchy.player.fx.MainWindowController;
+import com.paziy.sketchy.player.fx.View;
+import com.paziy.sketchy.request.controllers.VKUrlManager;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class ApplicationController {
+import java.io.IOException;
+import java.net.URL;
+
+public class ApplicationController extends Application {
 	private static ApplicationController instance;
+    private AudioPlayerController audioController;
 	
-	private ApplicationController() {
+	public ApplicationController() {
 	}
-	
-	public static ApplicationController getInstance() {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        ApplicationController.getInstance().run(stage);
+    }
+
+    public static ApplicationController getInstance() {
 		if (instance == null) {
 			instance = new ApplicationController();
 		}
@@ -17,13 +35,26 @@ public class ApplicationController {
 		return instance;
 	}
 	
-	public void run() {
+	public void run(Stage stage) throws IOException {
 		Authenticator auth = new LoginWindow();
-		
-		System.out.println(auth.login());
-	}
+
+        VKUrlManager manager = new VKUrlManager(auth.login());
+        audioController = new AudioPlayerController(manager);
+        URL location = View.class.getResource("MainWindow.fxml");
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(location);
+        Pane pane = (Pane) fxmlLoader.load();
+        MainWindowController controller = (MainWindowController)fxmlLoader.getController();
+        controller.setController(audioController);
+        controller.setRecords();
+        Scene scene = new Scene(pane);
+
+        stage.setScene(scene);
+        stage.show();
+    }
 	
 	public static void main(String[] args) {
-		ApplicationController.getInstance().run();
+		launch(args);
 	}
 }
